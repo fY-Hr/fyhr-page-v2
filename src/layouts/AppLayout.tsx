fe0fe5f439ddb2908f5c6ecb29453d48441e2280
fY-Hr <anteikuwute@gmail.com>
2026-07-02 21:37:06 +0700

    chore: scrollRestoration and overscroll background change

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -5,17 +5,18 @@ import { useRouterState } from '@tanstack/react-router'
 
 export function AppLayout({ children } : { children: ReactNode }) {
 
+
   const pathname = useRouterState({
     select: (state) => state.location.pathname
   })
 
   return (
-    <div className="h-screen w-full overflow-x-hidden text-[#bac3dd] bg-[#012182]">
+    <div className="min-h-screen w-full overflow-x-hidden text-[#bac3dd] bg-[#012182]">
       <div className="sm:w-11/12 md:w-3/4 lg:w-1/2 mx-auto">
         <header className="fixed top-4 left-1/2 -translate-x-1/2 z-50 w-full sm:w-full md:w-3/4 lg:w-1/2 px-4">
           <Navbar />
         </header>
-        <main className="h-full w-full flex flex-col overflow-y-auto overflow-x-hidden pt-24 px-8">
+        <main className="w-full flex flex-col overflow-x-hidden pt-24 px-8 ">
           <div key={pathname} className="w-full animate-fade-in">
             {children}
           </div>
